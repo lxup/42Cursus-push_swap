6727cbb7d946b6b077bbfe1101c5693a301550a3
Loup Quehec <lquehec@made-f0Ar8s5.clusters.42paris.fr>
2023-12-14 19:36:18 +0100

    fix se gfault

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: lquehec <lquehec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 18:18:26 by lquehec           #+#    #+#             */
-/*   Updated: 2023/12/13 20:14:22 by lquehec          ###   ########.fr       */
+/*   Updated: 2023/12/14 19:35:46 by lquehec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ void	ft_error(int error, char *param)
 {
 	if (error == EMPTY_ERR)
 		return ;
-	ft_putstr_fd("Error:\n", 2);
+	ft_putstr_fd("Error\n", 2);
 	if (error == MEMORY_ERR)
-		ft_putstr_fd("DAMN, no memory left on device", 2);
+		ft_putstr_fd("No memory left on device", 2);
 	if (error == ARGS_ERR)
 		ft_putstr_fd("Arguments-> ", 2);
 	if (error == SORT_ERR)
